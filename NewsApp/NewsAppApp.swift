a0e8c400a4aa39fd5123870e0ddf67352199551e
Harish Gajabheenkar <harishgajabheenkar@gmail.com>
2024-07-26 16:09:13 +0530

    Innitial commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Harish on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
